6b9ce6d2e6a2251279eacfd495213f0163982fcb
Eduardapac <eduardaleitepac@gmail.com>
2024-08-20 12:25:04 -0300

    20/08

--- src/main/java/br/com/eduarda/estoque/controllers/CategoriaController.java
+++ src/main/java/br/com/eduarda/estoque/controllers/CategoriaController.java
@@ -1,14 +1,15 @@
 package br.com.eduarda.estoque.controllers;
 
 import br.com.eduarda.estoque.model.Categoria;
+import br.com.eduarda.estoque.repositories.CategoriaRepository;
 import br.com.eduarda.estoque.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
@@ -18,6 +19,14 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaService categoriaService;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping()
+    public List<Categoria> listarTodasCategorias(){
+        return categoriaRepository.findAll(Sort.by("nome").ascending());
+    }
     @PostMapping()
     public ResponseEntity<Categoria> inserir(@RequestBody Categoria categoria) {
         Categoria categoriaSalva = categoriaService.salvar(categoria);
--- src/main/java/br/com/eduarda/estoque/controllers/ProdutoController.java
+++ src/main/java/br/com/eduarda/estoque/controllers/ProdutoController.java
@@ -1,14 +1,16 @@
 package br.com.eduarda.estoque.controllers;
 
+import br.com.eduarda.estoque.model.Categoria;
 import br.com.eduarda.estoque.model.Produto;
+import br.com.eduarda.estoque.repositories.ProdutoRepository;
 import br.com.eduarda.estoque.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -17,6 +19,14 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping()
+    public List<Produto> listarTodosProdutos(){
+        return produtoRepository.findAll(Sort.by("nomeproduto").ascending());
+    }
     @PostMapping()
     public ResponseEntity<Produto> inserir(@RequestBody Produto produto){
         Produto produtoSalvar = produtoService.salvar(produto);
