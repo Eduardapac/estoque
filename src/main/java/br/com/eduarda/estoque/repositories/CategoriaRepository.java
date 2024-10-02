40c3ec30a7a83181645110a9a57c6063a4a2905f
Eduardapac <eduardaleitepac@gmail.com>
2024-10-02 08:54:29 -0300

    02/10

--- src/main/java/br/com/eduarda/estoque/repositories/CategoriaRepository.java
+++ src/main/java/br/com/eduarda/estoque/repositories/CategoriaRepository.java
@@ -1,9 +1,10 @@
 package br.com.eduarda.estoque.repositories;
 
 import br.com.eduarda.estoque.model.Categoria;
+import br.com.eduarda.estoque.repositories.categoria.CategoriaRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>, CategoriaRepositoryQuery {
 }
