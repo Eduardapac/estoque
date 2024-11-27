6d54b9069e663ddf17b37e354e724255dd337e54
Eduardapac <eduardaleitepac@gmail>
2024-11-27 08:36:08 -0300

    27/11

--- src/main/java/br/com/eduarda/estoque/services/ProdutoService.java
+++ src/main/java/br/com/eduarda/estoque/services/ProdutoService.java
@@ -1,9 +1,13 @@
 package br.com.eduarda.estoque.services;
 
+import br.com.eduarda.estoque.model.Categoria;
 import br.com.eduarda.estoque.model.Produto;
 import br.com.eduarda.estoque.repositories.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService{
@@ -14,5 +18,21 @@ public class ProdutoService{
     public Produto salvar(Produto produto){
 
         return produtoRepository.save(produto);
+    }
+    public Produto atualizar(Integer id, Produto produto){
+        Produto produtoSalva = buscarProdutoExistente(id);
+
+        BeanUtils.copyProperties(produto, produtoSalva, "id");
+
+        return  produtoRepository.save(produtoSalva);
+    }
+
+    private Produto buscarProdutoExistente(Integer id) {
+        Optional<Produto> produtoSalva = produtoRepository.findById(id);
+
+        if(!produtoSalva.isPresent()) {
+            throw new IllegalArgumentException();
+        }
+        return produtoSalva.get();
     }
 }
